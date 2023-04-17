f8c67c60febd6b3bc991d89d3e33a3e6fc4d8f67
Dog_FootPrint <woobin2000@nate.com>
2023-04-17 16:32:12 +0900

    [Add] DBData CheckBox로 확인

--- JPGConverter.py
+++ JPGConverter.py
@@ -1,95 +1,163 @@
 from pymongo import MongoClient
 import matplotlib.pyplot as plt
-import pprint
 
-def ConvertToJPG() :
-    # Connect To DB / Make Figure 
-    mongodb_URI = "mongodb://admin:1229@svc.sel3.cloudtype.app:30778/?authMechanism=DEFAULT"
-    client = MongoClient(mongodb_URI)
-    db = client['Hynpytol']
+class Converter:
+    def __init__(self) -> None:
+        self.mongodb_URI = "mongodb://admin:1229@svc.sel3.cloudtype.app:30778/?authMechanism=DEFAULT"
+        self.pltTitle = ""
+                
+    def ConnectToDB(self) :
+        # Connect To DB
+        self.client = MongoClient(self.mongodb_URI)
+        self.db = self.client['Hynpytol']
 
-    #for문 사용시마다 초기화필요 (Cursor 자료형인듯)
-    UserDatas_documents = db['UserDatas'].find({})
+        #for문 사용시마다 초기화필요 (Cursor 자료형인듯)
+        self.UserDatas_documents = self.db['UserDatas'].find({})
 
-    #총 데이터 숫자를 세주는 듯
-    UserDatas_documents_count = db['UserDatas'].count_documents({})
-    UserList_documents_count = db['UserList'].count_documents({})
-    plt.figure(figsize=(20,20))
+        #총 데이터 숫자를 세주는 듯
+        self.UserDatas_documents_count = self.db['UserDatas'].count_documents({})
+        self.UserList_documents_count = self.db['UserList'].count_documents({})
+        plt.figure(figsize=(15,8))
 
-#region Mak PlayTime Plot
+    def AddPlaytimePlot(self):
+        # Make PlayTimeDictionary
+        playTimeDic = {}
+        checkCount = {}
 
-    # Make PlayTimeDictionary
-    playTimeDic = {}
-    playCount = {}
-
-    for i, document in enumerate(UserDatas_documents, 1):
+        self.UserDatas_documents = self.db['UserDatas'].find({})
+        for i, document in enumerate(self.UserDatas_documents, 1):
             camPos = document['CamPosName']
             playTime = document['PlayTime']
-            uuid = document['UUID']
             if camPos in playTimeDic :
                 playTimeDic[camPos] = playTimeDic[camPos] + playTime
-                playCount[camPos] = playCount[camPos] + 1     
+                checkCount[camPos] = checkCount[camPos] + 1     
             else :
                 playTimeDic[camPos] = playTime       
-                playCount[camPos] = 1     
+                checkCount[camPos] = 1     
 
-            
+        for key in playTimeDic:
+            playTimeDic[key] = playTimeDic[key] / checkCount[key]
 
-    for key in playTimeDic:
-        playTimeDic[key] = playTimeDic[key] / playCount[key]
-
-    # Average PlayTimePlot
-    plt.subplot(2,1,1)
-    x = []
-    xTicks = list(playTimeDic.keys())
-    for i in range(0,50):
-        x.append(i)
-    y = playTimeDic.values()
-    plt.xticks(x, xTicks)
+        # Average PlayTimePlot
+        y = playTimeDic.values()
    
-    plt.xlabel('Cam Pos Name')
-    plt.ylabel('Average Play Time')
-    plt.title(label='Total Data : ' + str(UserDatas_documents_count))
-    plt.plot(x,y)
+        self.pltTitle += 'Checked Data(PlyaTime) : ' + str(self.UserDatas_documents_count) + '/'
+        plt.plot(y , 'r' , label='Average PlayTime (s)')
 
-#endregion
+    def AddHookCountPlot(self):
+        hookCountDic = {}
+        checkCount = {}
 
+        self.UserDatas_documents = self.db['UserDatas'].find({})
+        for i, document in enumerate(self.UserDatas_documents, 1):
+            camPos = document['CamPosName']
+            hookCount = document['HookCount']
+            if camPos in hookCountDic :
+                hookCountDic[camPos] = hookCountDic[camPos] + hookCount
+                checkCount[camPos] = checkCount[camPos] + 1     
+            else :
+                hookCountDic[camPos] = hookCount       
+                checkCount[camPos] = 1     
 
-#region Make Player Count Plot
-    # Player Count
-    playerCount = {}
-    UserDatas_documents = db['UserDatas'].find({})
-    for document in UserDatas_documents:
-        camPos = document['CamPosName']
-        uuid = document['UUID']
-        if camPos not in playerCount :
-            playerCount[camPos] = []
-            playerCount[camPos] += [uuid]
-        else :
-            if uuid not in playerCount[camPos] :
+        for key in hookCountDic:
+            hookCountDic[key] = hookCountDic[key] / checkCount[key]
+
+        # Average PlayTimePlot
+        y = hookCountDic.values()
+   
+        self.pltTitle += 'Checked Data(HookCount) : ' + str(self.UserDatas_documents_count) + '/'
+        plt.plot(y , 'g' , label='Average HookCount')
+
+    def AddUndoCountPlot(self):
+        undoCountDic = {}
+        checkCount = {}
+
+        self.UserDatas_documents = self.db['UserDatas'].find({})
+        for i, document in enumerate(self.UserDatas_documents, 1):
+            camPos = document['CamPosName']
+            undoCount = document['UndoCount']
+            if camPos in undoCountDic :
+                undoCountDic[camPos] = undoCountDic[camPos] + undoCount
+                checkCount[camPos] = checkCount[camPos] + 1     
+            else :
+                undoCountDic[camPos] = undoCount       
+                checkCount[camPos] = 1     
+
+        for key in undoCountDic:
+            undoCountDic[key] = undoCountDic[key] / checkCount[key]
+
+        # Average PlayTimePlot
+        y = undoCountDic.values()
+   
+        self.pltTitle += 'Checked Data(UndoCount) : ' + str(self.UserDatas_documents_count) + '/'
+        plt.plot(y , 'b' , label='Average UndoCount')
+
+    def AddRetryCountPlot(self):
+        retryCountDic = {}
+        checkCount = {}
+
+        self.UserDatas_documents = self.db['UserDatas'].find({})
+        for i, document in enumerate(self.UserDatas_documents, 1):
+            camPos = document['CamPosName']
+            retryCount = document['RetryCount']
+            if camPos in retryCountDic :
+                retryCountDic[camPos] = retryCountDic[camPos] + retryCount
+                checkCount[camPos] = checkCount[camPos] + 1     
+            else :
+                retryCountDic[camPos] = retryCount       
+                checkCount[camPos] = 1     
+
+        for key in retryCountDic:
+            retryCountDic[key] = retryCountDic[key] / checkCount[key]
+
+        # Average PlayTimePlot
+        y = retryCountDic.values()
+   
+        self.pltTitle += 'Checked Data(RetryCount) : ' + str(self.UserDatas_documents_count) + '/'
+        plt.plot(y , 'c' , label='Average RetryCount')
+
+    def AddPlayerCountPlot(self):
+        playerCount = {}
+        UserDatas_documents = self.db['UserDatas'].find({})
+        for document in UserDatas_documents:
+            camPos = document['CamPosName']
+            uuid = document['UUID']
+            if camPos not in playerCount :
+                playerCount[camPos] = []
                 playerCount[camPos] += [uuid]
+            else :
+                if uuid not in playerCount[camPos] :
+                    playerCount[camPos] += [uuid]
+            
         
-    
 
-    plt.subplot(2,1,2)
-    plt.xticks(x, xTicks)
-    plt.yticks(range(0, UserList_documents_count + 1))
-    plt.xlabel('Cam Pos Name')
-    plt.ylabel('Player Count')
-    plt.ylim([0,UserList_documents_count])
-    y = []
-    for item in playerCount:
-        y.append(len(playerCount[item]))
+        y = []
+        for item in playerCount:
+            y.append(len(playerCount[item]))
 
-    plt.title(label='Total Player : ' + str(UserList_documents_count))
-    plt.plot(x,y)
+        self.pltTitle += 'Total Player : ' + str(self.UserList_documents_count) + '/'
+        plt.plot(y , 'm' , label='PlayerCount')
 
-    
-#endregion
+    def SavePlot(self):
+        
+        camPoses = []
+        UserDatas_documents = self.db['UserDatas'].find({})
+        for document in UserDatas_documents:
+            camPos = document['CamPosName']
+            if camPos not in camPoses :
+                camPoses.append(camPos)
 
-    # Save as jpg
-    plt.savefig('static/img/output.jpg')
+        x = []
+        for i in range(0,50):
+            x.append(i)
+        plt.xticks(x, camPoses)
 
-    return plt
+        plt.title(self.pltTitle)
+        plt.xlabel('Cam Pos Name')
+        plt.legend()
+        plt.savefig('static/img/output.jpg')
 
-ConvertToJPG()
+
+
+
+#ConvertToJPG()
--- app.py
+++ app.py
@@ -1,13 +1,50 @@
-from flask import Flask, render_template
-import JPGConverter as converter
+from flask import Flask, render_template, request
+import JPGConverter
+import os
 
 
 app = Flask(__name__)
+
+img = os.path.join('static', 'img')
  
-@app.route("/")
+@app.route('/', methods = ['GET' , 'POST'])
 def main():
-    converter.ConvertToJPG()
-    return render_template('VisualHTML.html')
+    cvt = JPGConverter.Converter()
+    cvt.ConnectToDB()
+    if(request.method == 'POST'):
+        
+        if request.form['Refresh'] == "Refresh":
+            if request.form.get('PlayTime'):
+                cvt.AddPlaytimePlot()
+
+            if request.form.get('HookCount'):
+                cvt.AddHookCountPlot()
+
+            if request.form.get('UndoCount'):
+                cvt.AddUndoCountPlot()
+
+            if request.form.get('RetryCount'):
+                cvt.AddRetryCountPlot()
+
+            if request.form.get('PlayerCount'):
+                cvt.AddPlayerCountPlot()
+
+            cvt.SavePlot()
+            file = os.path.join(img,'output.jpg')
+        return render_template('VisualHTML.html', image=file)
+        
+    else :
+        cvt.AddPlaytimePlot()
+        cvt.AddHookCountPlot()
+        cvt.AddUndoCountPlot()
+        cvt.AddRetryCountPlot()
+        cvt.AddPlayerCountPlot()
+
+        cvt.SavePlot()
+        file = os.path.join(img,'output.jpg')
+        return render_template('VisualHTML.html', image=file)
+
+
  
 if __name__ == '__main__':
     app.run()
